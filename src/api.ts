b11103c57d3a15f13015827b537a6cb2ce2692de
meoaa <sindaea77@gmail.com>
2023-02-17 12:53:25 +0900

    change api

--- src/api.ts
+++ src/api.ts
@@ -10,7 +10,10 @@ import {
 } from "./types";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://127.0.0.1:8000/api/v1"
+      : "https://backend.instaclone.site/api/v1",
   withCredentials: true,
 });
 
